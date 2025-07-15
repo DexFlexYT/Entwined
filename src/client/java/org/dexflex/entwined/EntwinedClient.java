9152691e161a24b5f387c1e7bccfb504518a8fb5
DexFlex_YT <142329830+DexFlexYT@users.noreply.github.com>
2025-07-15 17:41:55 +0400

    Initial commit

---
+++ src/client/java/org/dexflex/entwined/EntwinedClient.java
@@ -0,0 +1,10 @@
+package org.dexflex.entwined;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EntwinedClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
